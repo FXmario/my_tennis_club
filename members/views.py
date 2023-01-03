24ba450dacf9f2fdace6e769cf22e5ce084f4a0e
FXmario <fransiskus_mario87@outlook.com>
2023-01-03 15:39:57 +0700

    First commit

---
+++ members/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Members
+
+def members(request):
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers' : mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Members.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember' : mymember,
+    }
+    return HttpResponse(template.render(context, request))
